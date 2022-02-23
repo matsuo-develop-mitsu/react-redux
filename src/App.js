30724efe2a44eee04d0f6fad486f6d410dc3e4ad
matsuo-develop-mitsu <matsuo.develop+mitsu@gmail.com>
2022-02-23 12:19:43 +0900

    know how to use useEffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 const App = (props) => {
   const [state, setState] = useState(props);
   const { name, price } = state;
 
+  // useEffectはcomponet内に何個も書くことができる
+  useEffect(() => {
+    console.log("This is like component did mount or compoent did update");
+  });
+
+  // 空配列なので、最初の描画時のみに発火
+  useEffect(() => {
+    console.log("This is like component did mout");
+  }, []);
+
+  // 配列にnameを指定しているので、nameのpropsが変更された時のみに発火
+  useEffect(() => {
+    console.log("Name is like component did mout");
+  }, [name]);
+
   return (
     <>
       <p>
-        現在の{name}は、{price}円です
+        現在の{name}は、{price}円です。
       </p>
       <button onClick={() => setState({ ...state, price: price + 1 })}>
         +1
